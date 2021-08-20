56fe2367dfe85b2077ff650abd5d49ce17a24819
Mike Daly <mike-daly@users.noreply.github.com>
2021-08-20 15:45:50 -0700

    start scanning files for metadata

---
+++ file_info_to_db/file_info_to_db/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace file_info_to_db
+{
+    class Program
+    {
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        /// <param name="args">arguments</param>
+        static void Main(string[] args)
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+            Console.WriteLine("Hello World!");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.WriteLine("Debugger attached, press any key to exit session.");
+                Console.ReadKey();
+            }
+        }
+
+        static void DoHelp(String message)
+        {
+            if (message.Length >0)
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine(@"
+")
+        }
+
+    }
+}
