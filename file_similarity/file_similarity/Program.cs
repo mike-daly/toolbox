67b2eec5034cba3cf7d62c66e76357575cebe21f
Mike Daly <tbd>
2021-10-01 18:58:53 -0700

    adding missing files

---
+++ file_similarity/file_similarity/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace file_similarity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("press any key to exit");
+                Console.ReadKey();
+            }
+        }
+
+        private static void DoHelp(string s)
+        {
+            Console.WriteLine(@"
+{0}
+
+-help       this
+-debugger   start the debugger
+", s);
+
+        }
+    }
+}
